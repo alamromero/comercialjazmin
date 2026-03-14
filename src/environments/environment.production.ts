8268e4fc277b3f2ffe6806e5b5b475036e3093b0
Alam Romero <alamromero@gmail.com>
2026-03-14 16:57:21 -0600

    Add files via upload

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://uocupinischhofqidfyk.supabase.co/',
+  apiToken:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvY3VwaW5pc2NoaG9mcWlkZnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNjEyNDEsImV4cCI6MjA4NTYzNzI0MX0.HscoDIER0dXj2TErexTA_MJFC2xpS153KLrZ1gFYT0c',
+};
